d997386250f8906844894ffa482665ff50a12419
Anna Carolina Zomer <117862465+Anz0mer@users.noreply.github.com>
2023-04-10 20:51:36 +0000

    código

--- Teste.py
+++ Teste.py
@@ -1,1 +1,14 @@
-a
+from math import *
+
+altura_da_bola = 71.00
+velocidade_0_bola = 20.50
+angulo_bola = radians(35)
+
+cosseno = cos(angulo_bola)
+seno = sin(angulo_bola)
+
+v0x = velocidade_0_bola * cosseno
+v0y = velocidade_0_bola * seno
+
+print("%f" % v0x)
+print("%.2f" % v0y)
