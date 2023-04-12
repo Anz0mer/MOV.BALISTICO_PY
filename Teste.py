393761d3454bbb2e1e5e535884255109ee4178ea
Mary026 <128990569+Mary026@users.noreply.github.com>
2023-04-12 12:27:49 -0300

    Algumas questoes

--- Teste.py
+++ Teste.py
@@ -1,19 +1,32 @@
 from math import *
 
-altura_da_bola = float(input("Digite a altura da bola:" ))
+altura_da_bola = float(input("Digite a altura da bola: " ))
 velocidade_0_bola = float(input("Digite a velocidade 0 da bola: "))
-angulo_bola = radians(input('Digite o angulo da bola: '))                               
+angulo_bola = float(input('Digite o angulo da bola: '))                               
 
-gravidade = 10
+angulo_bola = radians(angulo_bola)
+gravidade = 10.00
 
 cosseno = cos(angulo_bola)
 seno = sin(angulo_bola)
 
+seno_ao_quadrado = angulo_bola ** 2
+cosseno_ao_quadrado = angulo_bola ** 2
+
+seno_ao_quadrado = sin(seno_ao_quadrado)
+cosseno_ao_quadrado = cos(cosseno_ao_quadrado)
+
 v0x = velocidade_0_bola * cosseno
 v0y = velocidade_0_bola * seno
 
-print("Velocidade de v0x: %.2f" % v0x)
-print("Velocidade de v0y: %.2f" % v0y)
+print("Velocidade de v0x: %.2f\n" % v0x)
+print("Velocidade de v0y: %.2f\n" % v0y)
 
-tempo_alcance_y = (2 * (v0x * seno))/gravidade
-print("Tempo de alcance de y: %.2f" % tempo_alcance_y)
+tempo_alcance_bola = (2 * velocidade_0_bola * seno) / gravidade
+print("Tempo de alcance da bola: %.2f\n" % tempo_alcance_bola)
+
+h = ((velocidade_0_bola ** 2) * (seno ** 2))/ (2 * gravidade)
+print('O ponto mais alto da trajetória (H): %.2f\n' % h)
+
+a = ((velocidade_0_bola ** 2) * (seno * 2))/gravidade
+print('Alcance horizontal da bola (A): %.2f\n' % a)
